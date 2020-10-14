be5b09e2b81ebc6def510d3d0c8a617a6472e548
DavidMaxwell616 <33132201+DavidMaxwell616@users.noreply.github.com>
2020-10-13 20:32:56 -0700

    update

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-function loadXMLDoc(issue) {
+function loadXMLDoc(issue,file) {
   var xmlhttp = new XMLHttpRequest();
   var doc= this.document;
   xmlhttp.onreadystatechange = function() {
@@ -8,34 +8,35 @@ function loadXMLDoc(issue) {
       LoadGigs(xmlDoc,issue);
     }
   };
-  xmlhttp.open("GET", "../assets/xml/gigs.xml", true);
+  xmlhttp.open("GET", file, true);
   xmlhttp.send();
 }
 
 function LoadGigs(xml, issue) {
 var x = xml.getElementsByTagName("gigs")[0];
 if(x!==undefined){
-var children = x.childNodes;
-var table = '<table>\n<tr>\n';
-for(var item=0;item<GigFields.length;item++)
-{
-  table+='<th>'+ GigFields[item]+ '</th>\n';
+  var children = x.childNodes;
+  var table = '<table>\n<tr>\n';
+  for(var item=0;item<GigFields.length;item++)
+  {
+    table+='<th>'+ GigFields[item]+ '</th>\n';
+  }
+  table+='</tr>';
+  for(var item=1;item<children.length;item+=2)
+  {
+  var issue_num = parseInt(children[item].getElementsByTagName('issue_num')[0].textContent);
+  if(issue_num===issue){
+    table += "<tr>\n";
+    for(var item2=0;item2<GigFields.length;item2++)
+  {
+  var cellContents = children[item].getElementsByTagName(GigFields[item2].toLowerCase())[0].textContent;
+  table+='<td>'+ cellContents +  '</td>';
+  }
+  table+='</tr>\n';
+  }
+  table+='</table>';
+  }
 }
-table+='</tr>';
-for(var item=1;item<children.length;item+=2)
-{
-var issue_num = parseInt(children[item].getElementsByTagName('issue_num')[0].textContent);
-if(issue_num===issue){
-  table += "<tr>\n";
-  for(var item2=0;item2<GigFields.length;item2++)
-{
-var cellContents = children[item].getElementsByTagName(GigFields[item2].toLowerCase())[0].textContent;
-table+='<td>'+ cellContents +  '</td>';
-}
-table+='</tr>\n';
-}
-table+='</table>';
-}
-}
+
 document.getElementById("gigs").innerHTML = table;
 }
